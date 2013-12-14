baccb57d94ba67f44bd2e45cc85bf7016fa2d11d
Gerald Kaszuba <gak@gakman.com>
2013-12-14 20:41:00 +0000

    Just need 8125 open.

--- fabfile.py
+++ fabfile.py
@@ -3,11 +3,7 @@ from fabric.api import *
 args = ' '.join([
     '-i',
     '-t',
-    '-p 127.0.0.1::80/tcp',
-    '-p 127.0.0.1:8125:8125/udp',
-#    '-p 127.0.0.1:2003:2003/udp',
-#    '-p 127.0.0.1:2004:2004/udp',
-#    '-p 127.0.0.1:7002:7002/udp',
+    '-p 8125:8125/udp',
     '-v /data/graphite:/var/lib/graphite/storage'
 ])
 
@@ -17,7 +13,7 @@ def build():
 
 
 def start():
-    local('sudo docker run {} graphite'.format(args))
+    local('sudo docker run {} -d graphite'.format(args))
 
 
 def shell():
